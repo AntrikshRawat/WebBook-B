f72bb053f229db822d133d2106e8eb88d62c2abb
Antriksh rawat <antrikshrawat2@gmail.com>
2024-08-20 18:42:32 +0530

    new

--- database.js
+++ database.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const mongoURI = "mongodb+srv://antrikshrawat2:<Antriksh@Rawat31>@ar-cluster.eio7o.mongodb.net/?retryWrites=true&w=majority&appName=AR-CLUSTER";
 
 const connetToMongo = async ()=>{
-          try{
+try{
 await mongoose.connect(mongoURI);
 console.log("connected successfully!");
-          }catch(error) {
-                    console.log(error);
-          }
+}catch(error) {
+          console.log(error);
+ }
 }
 
 module.exports = connetToMongo;
