dc6aa7d2d214f78f5e3e95905e04e40159705ff4
Antriksh rawat <antrikshrawat2@gmail.com>
2024-08-21 23:13:58 +0530

    new commit

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv://antrikshrawat2:antrikshrawat2@ar-cluster.eio7o.mongodb.net/?retryWrites=true&w=majority&appName=AR-CLUSTER";
+const mongoURI = "mongodb+srv://antrikshrawat2:antrikshrawat2@ar-cluster.eio7o.mongodb.net/WebBook?retryWrites=true&w=majority&appName=AR-CLUSTER";
 
 const connectToMongo = async ()=>{
 try{
